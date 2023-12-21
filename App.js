598527db20adcfd3542fa2a508248cdbf01e4601
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2023-12-21 15:45:08 +0530

    code setup + basic navigation

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import WorkoutScreen from './src/Screens/WorkoutScreen';
+import CalculationScreen from './src/Screens/CalculationScreen';
+import TimerScreen from './src/Screens/TimerScreen';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  function TabNavigator(){
+    return(
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarHideOnKeyboard: true,
+          tabBarIcon: ({color, size}) => {
+            let iconName;
+            switch(route.name){
+              case "Workout":
+              iconName= "dumbbell";
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+
+              case "Timer":
+              iconName= "timer-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+
+              case "Calculation":
+              iconName= "calculator-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+
+            }
+          },
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle:{
+            backgroundColor:"black",
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingVertical: 5,
+          },
+          tabBarActiveTintColor: "aqua",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name='Workout' component={WorkoutScreen}/>
+        <Tab.Screen name='Timer' component={TimerScreen}/>
+        <Tab.Screen name='Calculation' component={CalculationScreen}/>
+      </Tab.Navigator>
+    )
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='TabNav' component={TabNavigator}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Screens/CalculationScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CalculationScreen = () => {
+  return (
+    <View>
+      <Text>CalculationScreen</Text>
+    </View>
+  )
+}
+
+export default CalculationScreen
